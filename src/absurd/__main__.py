43f0667421a10552aa1ed7338af7fb0c54c35615
feline-felicity <85366926+feline-felicity@users.noreply.github.com>
2026-03-28 22:54:46 +0900

    Ignore part name in favor of auto-configuration based on SIB and SIGROW

--- src/absurd/__main__.py
+++ src/absurd/__main__.py
@@ -7,7 +7,6 @@ from .debugger import Ocd
 from .nvmdrivers import create_nvm_driver
 from .rspserver import RspServer
 from .updi import AddressWidth, UpdiClient, UpdiException, UpdiFeatures, UpdiRev1, KEY_NVMPROG
-from .deviceinfo import get_deviceinfo
 
 log = getLogger()
 handler = StreamHandler(sys.stderr)
@@ -28,22 +27,53 @@ def get_updi_features(nvm_version: str) -> UpdiFeatures:
     )
 
 
+FLASH_SIZE_LUT = {
+    0x91: 2 * 1024,
+    0x92: 4 * 1024,
+    0x93: 8 * 1024,
+    0x94: 16 * 1024,
+    0x95: 32 * 1024,
+    0x96: 64 * 1024,
+    0x97: 128 * 1024,
+}
+
+MEGA0_SIGS = {
+    0x93: (0x26, 0x2A),
+    0x94: (0x27, 0x26),
+    0x95: (0x30, 0x31),
+    0x96: (0x50, 0x51),
+}
+
+
+def get_flash_size(sig: bytes) -> int:
+    # With AVR8Xs, flash size can be determined by the middle byte of the signature
+    # ...except for 0x96. This denotes 48 KiB for Mega 0 series (SIG3 = 0x50/0x51)
+    if sig[1] == 0x96 and sig[2] in MEGA0_SIGS[0x96]:
+        return 48 * 1024
+    return FLASH_SIZE_LUT.get(sig[1], 0x20000)  # default: 128 KiB, largest possible with 2-byte PC
+
+
+def get_flash_offset(nvmver: str, sig: bytes) -> int:
+    # Except for NVM v0 (Tinies and Mega 0), flash is mapped at 0x800000 in UPDI space.
+    if nvmver != "0":
+        return 0x800000
+    # For NVM v0, we use data space mapping for UPDI access, i.e. 0x8000
+    # ...except for Mega 0 again (0x4000).
+    if sig[1] in MEGA0_SIGS and sig[2] in MEGA0_SIGS[sig[1]]:
+        return 0x4000
+    return 0x8000
+
+
 def main():
     parser = ArgumentParser(description="AVR Basic SerialUPDI Remote Debugger")
-    parser.add_argument("-p", "--part", help="MCU name (e.g. avr16ea48)", required=True)
+    parser.add_argument("-p", "--part", help="MCU name (e.g. avr16ea48)", default="auto") # TODO: currently unused. Cross-examination and manual override to be implemented.
     parser.add_argument("-P", "--port", help="Serial port used as SerialUPDI (e.g. COM5 or /dev/ttyS1)", required=True)
     parser.add_argument("-b", "--bps", help="Baud rate for communication (defaults to 115200)", type=int, default=115200)
     parser.add_argument("-r", "--rsp-port", help="TCP port number for RSP communcation with gdb", type=int, default=3333)
     parser.add_argument("-s", "--swbp", help="Enable software breakpoints (WARNING: incompatible with debuggee's IAP operations)", action="store_true")
     parser.add_argument("-v", "--verbose", help="Print more logs", action="store_true")
-    # parser.add_argument("-F", "--enable-flashing", help="Enable features that require modifying NVM contents", action="store_true")
     args = parser.parse_args()
-    try:
-        devinfo = get_deviceinfo(args.part)
-    except ValueError:
-        print("Part name not recognized")
-        exit(1)
-    
+
     if args.verbose:
         handler.setLevel(DEBUG)
 
@@ -63,20 +93,26 @@ def main():
         uc.store_csr(0x8, 0x59)
         uc.store_csr(0x8, 0x00)
         time.sleep(0.1)
-        signature = uc.load_burst(devinfo.signature_addr, burst=3)
-        revid = uc.load_direct(0x0F01)              # SYSCFG.REVID
-        
-        sig = signature.hex("-").upper()
-        rev = f"{chr((revid>>4)+64)}{revid&0x0F}" if revid & 0xF0 else chr(revid + 64)
+
         nvmver = sib[10]
         ocdver = sib[13]
         sibrev = sib[20:22]
 
         print(f"UPDI rev.{updiver}")
         print(f"SIB: {sib}")
-        print(f"Signature: {sig} (revision {rev})")
         print(f"NVM: v{nvmver} / OCD: v{ocdver}")
-        
+
+        # Provisional driver for signature reading. Flash offset is irrelevant at this point.
+        nvm_driver = create_nvm_driver(nvmver, uc, flash_offset=0x800000)
+        signature = nvm_driver.read_signature()
+        revid = uc.load_direct(0x0F01)  # SYSCFG.REVID has been consistently here for all AVR8Xs. So far.
+        rev = f"{chr((revid >> 4) + 64)}{revid & 0x0F}" if revid & 0xF0 else chr(revid + 64)
+        print(f"Signature: {signature.hex("-").upper()} (revision {rev})")
+        flash_size = get_flash_size(signature)
+        print(f"Flash size: {flash_size // 1024} KiB")
+        flash_offset = get_flash_offset(nvmver, signature)
+        print(f"UPDI flash offset: {flash_offset:#06x}")
+
         uc.store_csr(0x8, 0x59)
         uc.store_csr(0x8, 0x00)
         time.sleep(0.1)
@@ -94,14 +130,14 @@ def main():
     # main loop
     updi_features = get_updi_features(nvmver)
     updic = UpdiClient(args.port, args.bps, updi_prescaler=0, features=updi_features)
+    nvm_driver = create_nvm_driver(nvmver, updic, flash_offset)
     try:
-        nvm_driver = create_nvm_driver(nvmver, updic, devinfo.flash_offset)
-        dbg = Ocd(updic, flash_offset=devinfo.flash_offset, use_byte_pc=(ocdver == "0"))
+        dbg = Ocd(updic, flash_offset=flash_offset, use_byte_pc=(ocdver == "0"))
         sv = RspServer(args.rsp_port, dbg, nvm_driver, allow_swbp=args.swbp)
         log.info(f"Selected NVM driver for SIB NVM version {nvmver}")
         log.info("Starting RSP server...")
         sv.serve()
-    
+
     except UpdiException as ex:
         log.error(f"UPDI instruction `{ex.instruction}` failed")
         updic.disconnect()
